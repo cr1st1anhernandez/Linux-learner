02b75a13648bfcfc8a1e4c2168dcf30d1af8e95f
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-01-26 18:30:25 -0600

    :dizzy: Add animations to principal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
-'use client'
-import { Beaker } from '@/app/icons/Beaker'
-import { Github } from '@/app/icons/Github'
-import { LogIn } from '@/app/icons/LogIn'
-import { Button, Link } from '@nextui-org/react'
-import Image from 'next/image'
-import { useState } from 'react'
+"use client";
+import { Beaker } from "@/app/icons/Beaker";
+import { Github } from "@/app/icons/Github";
+import { LogIn } from "@/app/icons/LogIn";
+import { Button, Link } from "@nextui-org/react";
+import Image from "next/image";
 
 export default function Home(): JSX.Element {
-  const [isLoaded, setIsLoaded] = useState(false)
-
-  const handleLoad = (): void => {
-    setIsLoaded(!isLoaded)
-  }
-
   return (
     <section className="mx-auto flex flex-col items-center justify-center red-light md:w-5/6 xl:flex-row dark:red-dark">
-      <article className="flex h-fit flex-col justify-center gap-2 sm:gap-8 xl:w-1/2">
+      <article className="flex animate-fade-down animate-once h-fit flex-col justify-center gap-2 sm:gap-8 xl:w-1/2">
         <h1>¡Descubre el poder de linux desde cero!</h1>
         <p className="text-left text-zinc-500">
           Una herramienta que proporciona una base sólida para entrar al mundo
@@ -58,15 +51,14 @@ export default function Home(): JSX.Element {
           </Button>
         </div>
       </article>
-      <div className="flex w-full items-center justify-center md:w-2/3 xl:w-1/2">
+      <div className="flex w-full animate-fade-down animate-once items-center justify-center md:w-2/3 xl:w-1/2">
         <Image
           src="/images/principalImage.svg"
           width={800}
           height={800}
-          onLoad={handleLoad}
           alt="Imagen principal"
         />
       </div>
     </section>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,101 +1,102 @@
-import { nextui } from '@nextui-org/react'
-import type { Config } from 'tailwindcss'
+import { nextui } from "@nextui-org/react";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       gridTemplateColumns: {
-        'auto-fill-20': 'repeat(auto-fill, minmax(20rem, 1fr))',
+        "auto-fill-20": "repeat(auto-fill, minmax(20rem, 1fr))",
       },
     },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [
+    require("tailwindcss-animated"),
     nextui({
       themes: {
-        'red-dark': {
-          extend: 'dark',
+        "red-dark": {
+          extend: "dark",
           colors: {
-            background: '#101010',
-            foreground: '#d4d4d8',
+            background: "#101010",
+            foreground: "#d4d4d8",
             primary: {
-              50: '#fff1f2',
-              100: '#ffe4e6',
-              200: '#fecdd3',
-              300: '#fda4af',
-              400: '#fb7185',
-              500: '#f43f5e',
-              600: '#e11d48',
-              700: '#be123c',
-              800: '#9f1239',
-              900: '#881337',
-              DEFAULT: '#e11d48',
-              foreground: '#fff',
+              50: "#fff1f2",
+              100: "#ffe4e6",
+              200: "#fecdd3",
+              300: "#fda4af",
+              400: "#fb7185",
+              500: "#f43f5e",
+              600: "#e11d48",
+              700: "#be123c",
+              800: "#9f1239",
+              900: "#881337",
+              DEFAULT: "#e11d48",
+              foreground: "#fff",
             },
-            focus: '#be123c',
+            focus: "#be123c",
           },
           layout: {
-            disabledOpacity: '0.3',
+            disabledOpacity: "0.3",
             radius: {
-              small: '4px',
-              medium: '6px',
-              large: '8px',
+              small: "4px",
+              medium: "6px",
+              large: "8px",
             },
             borderWidth: {
-              small: '1px',
-              medium: '2px',
-              large: '3px',
+              small: "1px",
+              medium: "2px",
+              large: "3px",
             },
           },
         },
-        'red-light': {
-          extend: 'light',
+        "red-light": {
+          extend: "light",
           colors: {
-            background: '#fffffe',
-            foreground: '#000',
+            background: "#fffffe",
+            foreground: "#000",
             primary: {
-              50: '#881337',
-              100: '#9f1239',
-              200: '#be123c',
-              300: '#e11d48',
-              400: '#f43f5e',
-              500: '#fb7185',
-              600: '#fda4af',
-              700: '#fecdd3',
-              800: '#ffe4e6',
-              900: '#fff1f2',
-              DEFAULT: '#e11d48',
-              foreground: '#fff',
+              50: "#881337",
+              100: "#9f1239",
+              200: "#be123c",
+              300: "#e11d48",
+              400: "#f43f5e",
+              500: "#fb7185",
+              600: "#fda4af",
+              700: "#fecdd3",
+              800: "#ffe4e6",
+              900: "#fff1f2",
+              DEFAULT: "#e11d48",
+              foreground: "#fff",
             },
-            focus: '#be123c',
+            focus: "#be123c",
           },
           layout: {
-            disabledOpacity: '0.3',
+            disabledOpacity: "0.3",
             radius: {
-              small: '4px',
-              medium: '6px',
-              large: '8px',
+              small: "4px",
+              medium: "6px",
+              large: "8px",
             },
             borderWidth: {
-              small: '1px',
-              medium: '2px',
-              large: '3px',
+              small: "1px",
+              medium: "2px",
+              large: "3px",
             },
           },
         },
       },
     }),
   ],
-}
-export default config
+};
+export default config;
